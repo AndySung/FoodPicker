2d956f2eb1b1b3638ddd47e0a149adbaeb886247
AndySung <zongjian.song@nortekcontrol.com>
2023-09-15 12:44:21 +0800

    new project, first commit

---
+++ FoodPicker/FoodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerApp.swift
+//  FoodPicker
+//
+//  Created by Andy on 2023/9/15.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
