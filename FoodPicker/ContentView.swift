01e665b2774dcf0734915802bcee1350e1872bf1
AndySung <zongjian.song@nortekcontrol.com>
2023-09-15 18:56:25 +0800

    添加盘子图片，定义几个View

--- FoodPicker/ContentView.swift
+++ FoodPicker/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    let food = ["汉堡", "沙拉", "披萨", "意大利面", "鸡腿便当", "刀削面", "火锅", "牛肉面", "关东煮"]
+    @State private var selectedFood: String?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Image("dinner")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("今天吃啥子？")
+                .font(.title)
+                .bold()
+            Text(selectedFood ?? "还没设定")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.green)
+            Button("告诉我!") {
+                selectedFood = food.shuffled().filter{ $0 != selectedFood}.first
+            }.font(.title)
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
